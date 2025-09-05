0917d463f9a9b8c0527cd20ba00cde2b8760821a
Arijitkhan99git <arijitkhan52@gmail.com>
2025-09-05 17:09:06 +0530

    Flex wrap on Hero section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -7,7 +7,7 @@ import { motion } from "framer-motion";
 function Hero() {
   return (
     <section className='relative z-1  p-[2rem] text-white flex justify-center items-center pt-22 md:pt-25'>
-        <div className='w-full max-w-7xl p-[1rem] flex flex-col lg:flex-row justify-around gap-[3rem] '>
+        <div className='w-full max-w-7xl p-[1rem] flex flex-wrap flex-col lg:flex-row justify-around gap-[3rem] '>
              {/* right side */}
              <div className='flex flex-col justify-center items-center lg:justify-end lg:items-start gap-[3rem]  mx-auto lg:mx-0'>
            
